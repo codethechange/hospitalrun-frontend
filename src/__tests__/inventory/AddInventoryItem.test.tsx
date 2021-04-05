dc09aa637f0d9875892a9ba63f912c06ee2e91ba
jrmkim50 <jrmkim50@gmail.com>
2021-04-05 12:23:20 -0400

    working on addinventoryitem test

--- src/__tests__/inventory/AddInventoryItem.test.tsx
+++ src/__tests__/inventory/AddInventoryItem.test.tsx
@@ -1,5 +1,14 @@
+import React from 'react';
+import Inventory from '../../inventory/Inventory';
+import AddInventoryItem from '../../inventory/add/AddInventoryItem'
+
+import { render, screen, waitFor, waitForElementToBeRemoved, within } from '@testing-library/react'
+
 describe('AddInventoryItem', () => {
-  it('add item and cancel buttons render', () => {})
+  it('add item and cancel buttons render', () => {
+    const { container, getByText } = render(<AddInventoryItem/>)
+    console.log(container);
+  })
 
   it('cancel returns you to inventory page', () => {})
 
